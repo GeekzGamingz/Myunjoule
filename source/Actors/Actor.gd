a8a772d08f5707c6ec2ea54f822d33cb432286bb
Zeuk <geekzgamingz@gmail.com>
2022-08-13 15:16:39 -0400

    Added BG

--- source/Actors/Actor.gd
+++ source/Actors/Actor.gd
@@ -6,7 +6,7 @@ class_name Actor
 const FLOOR_NORMAL = Vector2.UP
 const SLOPE_SLIDE_STOP = 25.0
 var motion = Vector2()
-var gravity
+var gravity = 2
 var walk_speed = 3.5 * Globals.TILE_SIZE
 var run_speed = 7 * Globals.TILE_SIZE
 var max_speed = walk_speed
