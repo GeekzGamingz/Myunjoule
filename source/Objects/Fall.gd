7d84e21c05f35d94a798a8255c26dfa3c23146ba
Evan <evantellep@gmail.com>
2022-08-14 15:40:45 -0400

    Remove prints

--- source/Objects/Fall.gd
+++ source/Objects/Fall.gd
@@ -5,7 +5,5 @@ func _ready() -> void:
 	connect("body_entered", self, "toggle_falling")
 
 func toggle_falling(body: Node) -> void:
-	print("Entered fall area")
 	if body.has_method("toggle_is_falling"):
-		print("Body has method: 'toggle_is_falling'")
 		body.toggle_is_falling()
