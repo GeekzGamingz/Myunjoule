c02fbece14b4a8962a0b8e82e4fbfcf450748d82
Zeuk <geekzgamingz@gmail.com>
2022-08-18 14:20:42 -0400

    Cosmetic Changes; See Notes

--- source/UI/ProgressBars/ProgressBars.gd
+++ source/UI/ProgressBars/ProgressBars.gd
@@ -11,7 +11,10 @@ func energyUpdate_charge(energy) -> void:
 	energyTween.interpolate_property(energyOver, "value", energyOver.value, energy,
 								   0.4, Tween.TRANS_SINE, Tween.EASE_IN_OUT, 0.4)
 	energyTween.start()
+	$Regen.visible = true
 	$AnimationPlayer.play("regen")
+	yield($AnimationPlayer, "animation_finished")
+	$Regen.visible = false
 #Damage
 func energyUpdate_drain(energy) -> void:
 	energyOver.value = energy
