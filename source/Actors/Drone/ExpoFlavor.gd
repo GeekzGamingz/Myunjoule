c5be7d53644710a8e41eb14d2c3f325390ff1d83
Evan <evantellep@gmail.com>
2022-08-20 14:45:00 -0400

    Convert flavor text to use a Label

--- source/Actors/Drone/ExpoFlavor.gd
+++ source/Actors/Drone/ExpoFlavor.gd
@@ -2,11 +2,11 @@ extends Control
 
 var flavor
 
-onready var flavor_text = $MarginContainer/CenterContainer/RichTextLabel
-onready var tween = $MarginContainer/CenterContainer/Tween
+onready var flavor_text = $CenterContainer/Label
+onready var tween = $CenterContainer/Tween
 
 func _ready() -> void:
-	flavor_text.bbcode_text = flavor
+	flavor_text.text = flavor
 	flavor_text.percent_visible = 0
 	tween.interpolate_property(flavor_text, "percent_visible",
 		0, 1, 1.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
