e04eeaf46c757a410a12dfdd193f3bd5d479b8ef
Zeuk <geekzgamingz@gmail.com>
2022-08-21 06:29:28 -0400

    Slep Added to Drop Pod

--- source/Objects/DropPod/DropPod.gd
+++ source/Objects/DropPod/DropPod.gd
@@ -1,4 +1,13 @@
 extends StaticBody2D
+onready var player = get_tree().root.get_node("Moon/YSort/Player")
 
 func _ready() -> void:
 	$AnimationPlayer.play("phase_1")
+
+func next_dialog():
+	$DialogHandler.next_dialogue()
+
+#Temp
+func _on_Area2D_area_entered(area: Area2D) -> void:
+	if area.name == "PlayerArea":
+		player.is_slep = true
